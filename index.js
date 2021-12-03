fca0213ccba993dffa545790e2eea4d02421bc41
Andrew LeRoux <andrewleroux2639@gmail.com>
2021-12-03 13:22:19 -0800

    commit tree 7f3febda339118b9cc81c093fdf330c0a743cf4e

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-
+function superbowlWin(array) {
+    function isWin(element){
+        return element.result === "W"
+    }
+    
+    if (array.find(isWin) === undefined) {
+        return array.find(isWin)
+    }
+    else {
+        return array.find(isWin).year
+    }
+    
+    
+}
